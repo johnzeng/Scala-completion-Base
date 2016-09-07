09fb4ab10b1eb5b85905725643bcf8e89650a9b8
Johnzeng <Johnz@misfit.com>
2016-09-07 15:03:18 +0800

    add print all member codes

--- makefile
+++ makefile
@@ -1,9 +1,10 @@
 run: printer.jar
 	scalac -Xplugin:printer.jar -P:printMember:1:16 -nowarn test.scala 
 	scalac -Xplugin:printer.jar -P:printMember:12:13 -nowarn test.scala 
+	scalac -Xplugin:printer.jar -P:printMember:12:13 -nowarn test.scala 
 
 test: printer.jar
-	scalac -Xplugin:printer.jar -P:printMember:12:13 -nowarn test.scala 
+	scalac -Xplugin:printer.jar -nowarn test.scala 
 
 printer.jar: plug.scala
 	fsc -d classes plug.scala
--- plug.scala
+++ plug.scala
@@ -14,13 +14,15 @@ class PrintAllMembers(val global: Global) extends Plugin {
   val description = "print out all defined member's members"
   val components = List[PluginComponent](Component)
 
-  var line = 0
-  var col = 0
+  var line:Option[Int] = None
+  var col:Option[Int] = None
   override def processOptions(options: List[String], error: String => Unit) {
     for (option <- options) {
       val opList = option.split(":")
-      line = opList.head.toInt
-      col = opList.last.toInt
+      if(opList.size == 2){
+        line = Some(opList.head.toInt)
+        col = Some(opList.last.toInt)
+      }
     }
   }
   
@@ -42,57 +44,105 @@ class PrintAllMembers(val global: Global) extends Plugin {
 
         val treeList = allTrees(unit.body)
 
-        val matchList = treeList.filter{ t =>
-          t.pos.line <= line && t.pos.column <= col
-          }.toList.sortWith{(a,b) =>
-            if(a.pos.line == b.pos.line){
-              a.pos.column < b.pos.column
+        (line, col) match{
+          case (Some(line),Some(col)) =>{
+            val matchList = treeList.filter{ t =>
+              t.pos.line <= line && t.pos.column <= col
+              }.toList.sortWith{(a,b) =>
+                if(a.pos.line == b.pos.line){
+                  a.pos.column < b.pos.column
+                }else{
+                  a.pos.line < b.pos.line
+                }
+              }
+            val aboutLast = matchList filter{ t =>
+              t.pos.line == matchList.last.pos.line &&
+              t.pos.column == matchList.last.pos.column
+            }
+
+            val (isPackage,sufix) = aboutLast.find{ t=>
+              t.toString.startsWith("import")
+              }match{
+                case None => (false,"")
+                case Some(t) =>{
+                  val s =t.toString.split("\\.").last
+                  (true, s)
+                } 
+              }
+
+
+            if(isPackage){
+              aboutLast.foreach{ t=>
+                  t.symbol.tpe.members.find{_.toString.endsWith(sufix)}.foreach{ m =>
+                    println(startkey)
+                    println(m.tpe.members)
+                    System.exit(0)
+                  }
+                }
             }else{
-              a.pos.line < b.pos.line
+              aboutLast.find{ t =>
+                //it is still possible that you don't get any symbol or type at that position
+                null != t.symbol && null != t.symbol.tpe
+                }.foreach{ t=>
+                  if(false == t.tpe.members.isEmpty){
+                    println(startkey)
+                    println(t.tpe.members)
+                  }
+                  else if(false == t.symbol.tpe.members.isEmpty){
+                    println(startkey)
+                    println(t.symbol.tpe.members)
+                  }
+              }
+
             }
+            System.exit(0)
+
           }
-        val aboutLast = matchList filter{ t =>
-          t.pos.line == matchList.last.pos.line &&
-          t.pos.column == matchList.last.pos.column
+          case _ =>{
+            //should print all symbol, line and members
+            val sortedList = treeList.toList.filter{t=> 
+              ( !t.toString.startsWith("package <empty>") &&
+                !t.toString.startsWith("scala.AnyRef") &&
+                t.toString != "<empty>" &&
+                null !=t.symbol &&
+                null != t.symbol.tpe && 
+                false == t.symbol.tpe.members.isEmpty &&
+                t.symbol.toString != "<none>") ||
+              t.toString.startsWith("import")
+            }.sortWith{(a,b) =>
+              if(a.pos.line == b.pos.line){
+                a.pos.column < b.pos.column
+              }else{
+                a.pos.line < b.pos.line
+              }
+            }
+            def printAll(index:Int,lastPrintLine:Int,lastPrintCol:Int,lastImportLine:Int,lastImportStr:String){
+              if(index >= sortedList.size){
+                return
+              }
+              val t = sortedList(index)
+              if (t.pos.line == lastPrintLine && t.pos.column == lastPrintCol){
+                printAll(index + 1, lastPrintLine,lastPrintCol,lastImportLine, lastImportStr)
+              }else{
+                if (t.pos.line == lastImportLine){
+                  //handle import print here
+                  printAll(index+1, lastPrintLine,lastPrintCol, lastImportLine, lastImportStr)
+                }else if(t.toString.startsWith("import")){
+                  printAll(index+1, lastPrintLine,lastPrintCol,t.pos.line, t.toString.split(" ").last)
+                }else{
+                  println(s"${t.toString}:${t.pos.line}:${t.pos.column}")
+                  println(t.symbol.tpe.members)
+                  printAll(index+1,t.pos.line,t.pos.column,lastImportLine, lastImportStr)
+                }
+              }
+            }
+            printAll(0,-1,-1,-1,"")
+            System.exit(0)
+          }
         }
 
-        val (isPackage,sufix) = aboutLast.find{ t=>
-          t.toString.startsWith("import")
-          }match{
-            case None => (false,"")
-            case Some(t) =>{
-              val s =t.toString.split("\\.").last
-              (true, s)
-            } 
-          }
-
-
-        if(isPackage){
-          aboutLast.foreach{ t=>
-              t.symbol.tpe.members.find{_.toString.endsWith(sufix)}.foreach{ m =>
-                println(startkey)
-                println(m.tpe.members)
-                System.exit(0)
-              }
-            }
-        }else{
-          aboutLast.find{ t =>
-            //it is still possible that you don't get any symbol or type at that position
-            null != t.symbol && null != t.symbol.tpe
-            }.foreach{ t=>
-              if(false == t.tpe.members.isEmpty){
-                println(startkey)
-                println(t.tpe.members)
-              }
-              else if(false == t.symbol.tpe.members.isEmpty){
-                println(startkey)
-                println(t.symbol.tpe.members)
-              }
-          }
-
-        }
-        System.exit(0)
       }
     }
   }
+
 }
